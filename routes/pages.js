757f08e9a035f4c87f71e3ddc15bc55b5768e2e5
Mehul Kaushal <mehulkaushal2002@gmail.com>
2022-04-24 04:40:17 +0530

    Hel added

--- routes/pages.js
+++ routes/pages.js
@@ -17,6 +17,51 @@ router.get('/login', (req, res) => {
     res.render("login");
 
 });
+router.get('/health', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        console.log(req.message);
+        res.render("health",{
+            user: req.user,
+        
+        });
+
+    }
+    else{
+        res.redirect('/login');
+    }
+
+
+});
+router.get('/edu', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        console.log(req.message);
+        res.render("edu",{
+            user: req.user,
+        
+        });
+
+    }
+    else{
+        res.redirect('/login');
+    }
+
+
+});
+router.get('/life', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        console.log(req.message);
+        res.render("life",{
+            user: req.user,
+        
+        });
+
+    }
+    else{
+        res.redirect('/login');
+    }
+
+
+});
 router.get('/profile', authController.isLoggedIn, (req, res) => {
     if (req.user) {
         console.log(req.message);
@@ -32,4 +77,5 @@ router.get('/profile', authController.isLoggedIn, (req, res) => {
 
 
 });
+
 module.exports = router;
